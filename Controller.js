625f9a9633dc162386776e19d121dc8684462196
Marwa <marwa3hmed@gmail.com>
2017-06-10 02:03:42 +0200

    Simple Demo Using AngularJS

---
+++ Controller.js
@@ -0,0 +1,15 @@
+﻿angular.module('myApp', []).controller('personCtrl', function ($scope)
+{
+    $scope.checkInteredNum = function ()
+    {
+        if (($scope.userInteredNum == 6) || ($scope.userInteredNum == 8))
+        {
+            $scope.result = "Correct :)";
+        }
+        else
+        {
+            $scope.result = "Incorrect!";
+        }
+    }
+    
+});
